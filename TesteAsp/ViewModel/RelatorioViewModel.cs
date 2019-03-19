6d178eba3081a1931c7c900a476638c318f4db01
Gabriel Rodrigues <gsrodrigues2@ucs.br>
2019-03-18 22:49:07 -0300

    Filtro de Pesquisa e Relatorio

---
+++ TesteAsp/ViewModel/RelatorioViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TesteAsp.ViewModel
+{
+    public class RelatorioViewModel
+    {
+        public int ID { get; set; }
+        public string TipoId { get; set; }
+        public string ProdDescricao { get; set; }
+        public string TipoDescricao { get; set; }
+        public float Quantidade { get; set; }
+        public string Unidade { get; set; }
+    }
+
+    public class SearchViewModel
+    {
+        public string cod_tipo { get; set; }
+        public string unidade { get; set; }
+    }
+
+}
